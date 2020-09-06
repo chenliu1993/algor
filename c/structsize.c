03e4095605e895934c7c4e276ade8503cd6ff711
cliu2 <cliu2@localhost.localdomain>
2020-09-06 00:22:07 -0700

    add a struct size count test

---
+++ c/structsize.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+    https://blog.csdn.net/shanghx_123/article/details/79679726
+    if pack(4) then it is the default settings.
+*/
+#pragma pack(2)
+typedef struct _TestStruct {
+    char a;
+    int b;
+    char c;
+    short d;
+} TestStruct;
+
+int
+main(int argc, char *argv[]) {
+    printf("the size of stuct is %d, the align is %d\n", sizeof(TestStruct), __alignof__(TestStruct));
+    return 0;
+}
