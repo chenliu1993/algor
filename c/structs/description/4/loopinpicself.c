fe5029bf53b6a7e5c76f8d1ef09d258d4bd87902
cliu2 <cliu2@localhost.localdomain>
2020-09-08 06:40:49 -0700

    self loop in pic

---
+++ c/structs/description/4/loopinpicself.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define arraysize(a) \
+    (sizeof(a)/sizeof*(a))
+#define VertexNum  7
+
+int g[VertexNum][VertexNum];
+int Visited[VertexNum];
+
+void
+checkLoop() {
+    // init pic
+    for(int i=0;i<VertexNum;i++){
+        Visited[i]=0;
+        for(int j=0;j<VertexNum;j++) {
+            g[i][j]=-1;
+        }
+    }
+    for(int i=0;i<VertexNum;i++) {
+        for(int j=0;j<VertexNum;j++) {
+            int c;
+            scanf("%d", &c);
+            g[i][j]=c;
+        }
+    }
+    // check
+    int single=1, isLoop=1;
+    for(int n=0;n<VertexNum;n++) {
+        isLoop=1;
+        for(int i=0;i<VertexNum;i++) {
+            single=1;
+            if(!Visited[i]) {
+                for(int j=0;j<VertexNum;j++) {
+                    if(g[j][i]!=-1) {
+                        single = 0;
+                    }
+                }
+                if(single) { // i is single, delete i
+                    for(int k=0;k<VertexNum;k++) {
+                        g[i][k]=-1;
+                    }    
+                    Visited[i]=1;
+                }
+            }
+        }
+        for(int i=0;i<VertexNum;i++) {
+            if(!Visited[i]){
+                isLoop=0;
+            }
+        }
+        if(isLoop) {
+            break;
+        }
+    }
+    if(isLoop) {
+        printf("loop\n");
+    }
+}
