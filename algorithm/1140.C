db33a7baabb24eab96742889d19cff75a3d78421
chenliu1993 <13630583107@163.com>
2020-10-14 23:39:25 -0700

    add 1140

---
+++ algorithm/1140.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int stoneGameII(int *piles, int n)
+{
+    int i, x, m, dp[110][110] = {0}, sum = 0;
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        sum += piles[i];
+        for (m = 1; m <= 50; m++)
+        {
+            if (i + 2 * m >= n)
+            {
+                dp[i][m] = sum;
+                continue;
+            }
+            for (x = 1; i + x <= n && x <= 2 * m; x++)
+            {
+                dp[i][m] = max(dp[i][m], sum - dp[i + x][max(x, m)]);
+            }
+        }
+    }
+
+    return dp[0][1];
+}
