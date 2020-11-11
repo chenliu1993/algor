91c3a29e22d5b507e2eae4c3f923cb6ad9e2f40d
chenliu1993 <13630583107@163.com>
2020-11-10 17:22:00 -0800

    add some comments to 231

--- algorithm/231.c
+++ algorithm/231.c
@@ -3,12 +3,16 @@
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>
+/**
+ * 如何获取二进制中最右边的 1：x & (-x)。
+ * 如何将二进制中最右边的 1 设置为 0：x & (x - 1)。
+**/
 int isPowerOfTwo(int n)
 {
     if (n == 0)
         return 0;
     long x = n;
-    return (x & (x - 1)) == 0;
+    return (x & (-1 * x)) == x;
 }
 int main(int argc, char *argv[])
 {
