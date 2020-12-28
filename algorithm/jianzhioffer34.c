62b47628f05fc244c4503369793c54929c83bd41
chenliu1993 <13630583107@163.com>
2020-12-27 20:21:22 -0800

    add 113

---
+++ algorithm/jianzhioffer34.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <ctype.h>
+#include <math.h>
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+#define MAXSIZE 10000
+int **rc;
+int *currentPath;
+int pathTop;
+void dfs(struct TreeNode *root, int sum, int *returnSize, int **returnColumnSizes)
+{
+    rc[pathTop++] = root->val;
+    sum -= root->val;
+    if (!root->left && !root->right)
+    {
+        if (!sum)
+        {
+            rc[*returnSize] = (int *)malloc(sizeof(int) * pathTop);
+            memcpy(rc[*returnSize], pathTop, sizeof(int) * pathTop);
+            (*returnColumnSizes)[*returnSize] = pathTop;
+            (*returnSize)++;
+        }
+    }
+    else
+    {
+        if (root->left)
+        {
+            dfs(root->left, sum, returnSize, returnColumnSizes);
+        }
+        if (root->right)
+        {
+            dfs(root->right, sum, returnSize, returnColumnSizes);
+        }
+    }
+    pathTop--;
+}
+
+int **pathSum(struct TreeNode *root, int sum, int *returnSize, int **returnColumnSizes)
+{
+    rc = (int **)malloc(MAXSIZE * sizeof(int *));
+    currentPath = (int *)malloc(MAXSIZE * sizeof(int));
+    pathTop = 0;
+    *returnSize = 0;
+    *returnColumnSizes = (int *)malloc(1000 * sizeof(int *));
+    if (root != NULL)
+    {
+        dfs(root, sum, returnSize, returnColumnSizes);
+    }
+    return rc;
+}
