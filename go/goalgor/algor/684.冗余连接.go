27ded1f0e052fce35724cd522f1a067abb787b59
Liu Chen <13630583107@163.com>
2021-10-22 12:49:23 +0800

    add 684 (bingchaji way

--- go/goalgor/algor/684.冗余连接.go
+++ go/goalgor/algor/684.冗余连接.go
@@ -5,73 +5,107 @@
  */
 
 // @lc code=start
-type graph struct {
-	child   []int
-	degrees map[int]int
-	adj     map[int][]int
-}
+// type graph struct {
+// 	child   []int
+// 	degrees map[int]int
+// 	adj     map[int][]int
+// }
 
-func constructG(edges [][]int) graph {
-	g := graph{
-		child: []int{},
-	}
-	g.degrees = map[int]int{}
-	g.adj = map[int][]int{}
-	for i := 0; i < len(edges); i++ {
-		g.degrees[edges[i][0]] = g.degrees[edges[i][0]] + 1
-		g.degrees[edges[i][1]] = g.degrees[edges[i][1]] + 1
-		g.adj[edges[i][0]] = append(g.adj[edges[i][0]], edges[i][1])
-		g.adj[edges[i][1]] = append(g.adj[edges[i][1]], edges[i][0])
-	}
-	for i := 1; i < len(g.degrees)+1; i++ {
-		if g.degrees[i] == 1 {
-			g.child = append(g.child, i)
-		}
-	}
-	return g
-}
+// func constructG(edges [][]int) graph {
+// 	g := graph{
+// 		child: []int{},
+// 	}
+// 	g.degrees = map[int]int{}
+// 	g.adj = map[int][]int{}
+// 	for i := 0; i < len(edges); i++ {
+// 		g.degrees[edges[i][0]] = g.degrees[edges[i][0]] + 1
+// 		g.degrees[edges[i][1]] = g.degrees[edges[i][1]] + 1
+// 		g.adj[edges[i][0]] = append(g.adj[edges[i][0]], edges[i][1])
+// 		g.adj[edges[i][1]] = append(g.adj[edges[i][1]], edges[i][0])
+// 	}
+// 	for i := 1; i < len(g.degrees)+1; i++ {
+// 		if g.degrees[i] == 1 {
+// 			g.child = append(g.child, i)
+// 		}
+// 	}
+// 	return g
+// }
 
-func isTree(g graph) bool {
-	if len(g.child) == 0 {
-		return false
-	}
-	var (
-		front int
-		rear  int
-	)
-	front = 0
-	rear = len(g.child)
-	for front != rear {
-		end := rear
-		for i := front; i < end; i++ {
-			for j := 0; j < len(g.adj[g.child[i]]); j++ {
-				g.degrees[g.adj[g.child[i]][j]] = g.degrees[g.adj[g.child[i]][j]] - 1
-				if g.degrees[g.adj[g.child[i]][j]] == 1 {
-					g.child = append(g.child, g.adj[g.child[i]][j])
-					rear = rear + 1
-				}
-			}
-			front = end
-		}
-	}
-	if len(g.degrees) != len(g.child) {
-		// Number of nodes does not meet
-		return false
-	}
-	return true
-}
+// func isTree(g graph) bool {
+// 	if len(g.child) == 0 {
+// 		return false
+// 	}
+// 	var (
+// 		front int
+// 		rear  int
+// 	)
+// 	front = 0
+// 	rear = len(g.child)
+// 	for front != rear {
+// 		end := rear
+// 		for i := front; i < end; i++ {
+// 			for j := 0; j < len(g.adj[g.child[i]]); j++ {
+// 				g.degrees[g.adj[g.child[i]][j]] = g.degrees[g.adj[g.child[i]][j]] - 1
+// 				if g.degrees[g.adj[g.child[i]][j]] == 1 {
+// 					g.child = append(g.child, g.adj[g.child[i]][j])
+// 					rear = rear + 1
+// 				}
+// 			}
+// 			front = end
+// 		}
+// 	}
+// 	if len(g.degrees) != len(g.child) {
+// 		// Number of nodes does not meet
+// 		return false
+// 	}
+// 	return true
+// }
 
+// func findRedundantConnection(edges [][]int) []int {
+// 	var (
+// 		i int
+// 		g graph
+// 	)
+// 	for i = len(edges) - 1; i >= 0; i-- {
+// 		tempEdges := make([][]int, len(edges[:i]))
+// 		copy(tempEdges, edges[:i])
+// 		tempEdges = append(tempEdges, edges[i+1:]...)
+// 		g = constructG(tempEdges)
+// 		if isTree(g) {
+// 			break
+// 		}
+// 	}
+// 	return edges[i]
+// }
 func findRedundantConnection(edges [][]int) []int {
 	var (
-		i int
-		g graph
+		i     int
+		idx1  int
+		idx2  int
+		fa    []int
+		find  func(int) int
+		union func(int, int)
 	)
-	for i = len(edges) - 1; i >= 0; i-- {
-		tempEdges := make([][]int, len(edges[:i]))
-		copy(tempEdges, edges[:i])
-		tempEdges = append(tempEdges, edges[i+1:]...)
-		g = constructG(tempEdges)
-		if isTree(g) {
+	find = func(target int) int {
+		if fa[target] == target {
+			return target
+		}
+		fa[target] = find(fa[target])
+		return fa[target]
+	}
+	union = func(x, y int) {
+		fa[find(x)] = find(y)
+	}
+	fa = make([]int, len(edges)+1)
+	for i = 1; i < len(edges)+1; i++ {
+		fa[i] = i
+	}
+	for i = 0; i < len(edges); i++ {
+		idx1 = edges[i][0]
+		idx2 = edges[i][1]
+		if find(idx1) != find(idx2) {
+			union(idx1, idx2)
+		} else {
 			break
 		}
 	}
