b22ffff2dee629e8e7f786f938e8e6559dedab3c
chenliu1993 <13630583107@163.com>
2021-12-20 20:24:17 +0800

    again for 525

--- main.go
+++ main.go
@@ -10,56 +10,66 @@ func Max(x, y int) int {
 	}
 	return x
 }
-func Min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func maxSubarraySumCircular(nums []int) int {
+func findMaxLength(nums []int) int {
 	n := len(nums)
-	if n == 1 {
-		return nums[0]
-	}
 	var (
-		ans                         int
-		sum                         int
-		minSums1, minSums2, maxSums []int
+		sum    int
+		maxLen int
+		dict   map[int]int
+		record map[int]int
 	)
-	maxSums = make([]int, n)
-	minSums1 = make([]int, n)
-	minSums2 = make([]int, n)
-	sum = 0
-	maxSums[0] = nums[0]
-	minSums1[0] = nums[0]
-	for i := 0; i < n; i++ {
-		sum = sum + nums[i]
+	maxLen = 0
+	record = map[int]int{
+		0: -1,
 	}
-	ans = -30001
-	for i := 1; i < n; i++ {
-		maxSums[i] = Max(maxSums[i-1]+nums[i], nums[i])
-		ans = Max(ans, maxSums[i])
-		if i < n-1 {
-			minSums1[i] = Min(minSums1[i-1]+nums[i], nums[i])
-			ans = Max(ans, sum-minSums1[i])
+	dict = map[int]int{
+		1: 1,
+		0: -1,
+	}
+	sum = 0
+	for i := 0; i < n; i++ {
+		sum = sum + dict[nums[i]]
+		if _, ok := record[sum]; !ok {
+			record[sum] = i
+		} else {
+			maxLen = Max(maxLen, i-record[sum])
 		}
 	}
-	minSums2[1] = nums[1]
-	for i := 2; i < n; i++ {
-		minSums2[i] = Min(minSums2[i-1]+nums[i], nums[i])
-		ans = Max(ans, sum-minSums2[i])
-	}
-	return ans
+	return maxLen
 }
 
 func main() {
-	nums := []int{-2}
-	// nums := []int{1, 2, 3}
+	nums := []int{0, 1}
+	// nums := []int{0, 1}
 	// defer func() {
 	// 	if err := recover(); err != nil {
 	// 		log.Println(err)
 	// 	}
 	// }()
-	fmt.Println(maxSubarraySumCircular(nums))
+	fmt.Println(findMaxLength(nums))
 }
+
+// Over time limit
+// func findMaxLength(nums []int) int {
+// 	n := len(nums)
+// 	var (
+// 		maxLen int
+// 		sums   []int
+// 	)
+// 	maxLen = 0
+// 	sums = make([]int, n)
+// 	sums[0] = nums[0]
+// 	for i := 1; i < n; i++ {
+// 		sums[i] = sums[i-1] + nums[i]
+// 		if sums[i]*2 == i+1 {
+// 			maxLen = Max(maxLen, i+1)
+// 			continue
+// 		}
+// 		for j := i - 1; j >= 0; j-- {
+// 			if (sums[i]-sums[j])*2 == i-j {
+// 				maxLen = Max(maxLen, i-j)
+// 			}
+// 		}
+// 	}
+// 	return maxLen
+// }
