6706de824c95519ae618ee4894196b2850d76cca
chenliu1993 <13630583107@163.com>
2021-12-31 11:42:57 +0800

    add standard answer for 649

--- go/goalgor/algor/649.dota-2-参议院.go
+++ go/goalgor/algor/649.dota-2-参议院.go
@@ -64,3 +64,31 @@ func predictPartyVictory(senate string) string {
 
 // @lc code=end
 
+// func predictPartyVictory(senate string) string {
+// 	n := len(senate)
+// 	var (
+// 		r, d []int
+// 	)
+// 	r = []int{}
+// 	d = []int{}
+// 	for i := 0; i < n; i++ {
+// 		if senate[i] == 'R' {
+// 			r = append(r, i)
+// 		} else {
+// 			d = append(d, i)
+// 		}
+// 	}
+// 	for len(r) > 0 && len(d) > 0 {
+// 		if r[0] < d[0] {
+// 			r = append(r, r[0]+n)
+// 		} else {
+// 			d = append(d, d[0]+n)
+// 		}
+// 		r = r[1:]
+// 		d = d[1:]
+// 	}
+// 	if len(r) > 0 {
+// 		return "Radiant"
+// 	}
+// 	return "Dire"
+// }
--- main.go
+++ main.go
@@ -4,61 +4,33 @@ import (
 	"fmt"
 )
 
-var dict = map[string]string{
-	"R": "Radiant",
-	"D": "Dire",
-}
-
 func predictPartyVictory(senate string) string {
 	n := len(senate)
-	if n == 1 {
-		return dict[senate]
-	}
 	var (
-		id         string
-		prohibited []int
+		r, d []int
 	)
-	for {
-		n = len(senate)
-		prohibited = make([]int, len(senate))
-		for i := 0; i < n; i++ {
-			if prohibited[i] == 1 {
-				continue
-			}
-			j := i + 1
-			j = j % n
-			for j != i {
-				if prohibited[j] != 1 {
-					if senate[j] != senate[i] {
-						prohibited[j] = 1
-						break
-					}
-				}
-				j = j + 1
-				j = j % n
-			}
-		}
-		tempSenates, rcount, dcount := "", 0, 0
-		for i := 0; i < n; i++ {
-			if prohibited[i] != 1 {
-				if senate[i] == 'R' {
-					rcount++
-				} else {
-					dcount++
-				}
-				tempSenates += string(senate[i])
-			}
-		}
-		senate = tempSenates
-		if rcount == n {
-			id = "R"
-			break
-		} else if dcount == n {
-			id = "D"
-			break
+	r = []int{}
+	d = []int{}
+	for i := 0; i < n; i++ {
+		if senate[i] == 'R' {
+			r = append(r, i)
+		} else {
+			d = append(d, i)
 		}
 	}
-	return dict[id]
+	for len(r) > 0 && len(d) > 0 {
+		if r[0] < d[0] {
+			r = append(r, r[0]+n)
+		} else {
+			d = append(d, d[0]+n)
+		}
+		r = r[1:]
+		d = d[1:]
+	}
+	if len(r) > 0 {
+		return "Radiant"
+	}
+	return "Dire"
 }
 
 func main() {
