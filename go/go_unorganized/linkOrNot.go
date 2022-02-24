4cee9be70a71e9df00852d296f5a01dd2679c1ca
chenliu1993 <13630583107@163.com>
2022-02-24 16:26:11 +0800

    Check if it is a link file by Lstat

---
+++ go/go_unorganized/linkOrNot.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+)
+
+// IsLink checks if the file is a link
+func IsLink(path string) (string, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	fmt.Println(fileInfo.Name())
+	fmt.Println(fileInfo.Mode()&fs.ModeSymlink == fs.ModeSymlink)
+	return fileInfo.Mode().String(), nil
+}
+
+func main() {
+	res, err := IsLink("/home/workspace/shiyuelc/ltl")
+	if err != nil {
+		return
+	}
+	fmt.Println(res)
+}
