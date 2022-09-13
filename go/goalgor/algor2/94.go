cc8c1ca863df24899de04066c52e7eb96b9886be
chenliu1993 <chenliu1993>
2022-09-13 21:37:23 +0800

    add simple 94 dfs

---
+++ go/goalgor/algor2/94.go
@@ -0,0 +1,19 @@
+package algor2
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	ans := append([]int{}, inorderTraversal(root.Left)...)
+	ans = append(ans, root.Val)
+	ans = append(ans, inorderTraversal(root.Right)...)
+	return ans
+}
